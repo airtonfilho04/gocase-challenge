ad396311f24e03a12fa33f320c595d2180db9197
airtonfilho04 <airtonfilhocontato@outlook.com>
2020-06-05 13:14:16 -0300

    Creating the entities

---
+++ db/migrate/20200605161110_create_orders.rb
@@ -0,0 +1,17 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.string :reference
+      t.string :purchase_channel
+      t.string :client_name
+      t.string :address
+      t.string :delivery_service
+      t.string :total_value
+      t.text :line_items
+      t.integer :status
+      t.references :batch, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
